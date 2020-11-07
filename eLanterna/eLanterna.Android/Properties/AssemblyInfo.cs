a74cbeff9fe3537053a116ae92e5c8d84b106067
EuardB <edi.boboc33@gmail.com>
2020-11-08 00:49:57 +0200

    Creare functionalitate simpla.

--- eLanterna/eLanterna.Android/Properties/AssemblyInfo.cs
+++ eLanterna/eLanterna.Android/Properties/AssemblyInfo.cs
@@ -6,14 +6,14 @@ using Android.App;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("eLanterna.Android")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("eLanterna")]
+[assembly: AssemblyDescription("O simpla aplicatie de aprins si stins lanterna de la telefon.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("eLanterna.Android")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCompany("EduardBoboc")]
+[assembly: AssemblyProduct("eLanterna")]
+[assembly: AssemblyCopyright("Copyright Eduard Boboc © 2020")]
 [assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyCulture("ro-ro")]
 [assembly: ComVisible(false)]
 
 // Version information for an assembly consists of the following four values:
--- eLanterna/eLanterna/MainPage.xaml.cs
+++ eLanterna/eLanterna/MainPage.xaml.cs
@@ -3,16 +3,45 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using Xamarin.Essentials;
 using Xamarin.Forms;
 
 namespace eLanterna
 {
     public partial class MainPage : ContentPage
     {
+        enum StareLanterna
+        {
+            Aprinsa,
+            Stinsa
+        }; StareLanterna stare;
+
         public MainPage()
         {
             InitializeComponent();
+            stare = StareLanterna.Stinsa;
+        }
+
+        private void btnLanterna_Clicked(object sender, EventArgs e)
+        {
+            if(stare == StareLanterna.Stinsa)
+            {
+                btnLanterna.Text = "Stinge lanterna";
+                btnLanterna.BackgroundColor = Color.FromHex("#57c4f2");
+                btnLanterna.TextColor = Color.FromHex("#0c1b21");
+                Flashlight.TurnOnAsync();
+                stare = StareLanterna.Aprinsa;
+            }
+            else
+            {
+                btnLanterna.Text = "Aprinde lanterna";
+                btnLanterna.BackgroundColor = Color.FromHex("#222222");
+                btnLanterna.TextColor = Color.FromHex("#f5f5f5");
+                Flashlight.TurnOffAsync();
+                stare = StareLanterna.Stinsa;
+            }
         }
     }
 }
